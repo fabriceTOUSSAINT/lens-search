c066e068dc5507da4b1fa2eca8d7f0b13719c47b
Fabrice Toussaint <Fabrice@macs-MBP.fios-router.home>
2016-08-21 14:28:58 -0400

    Success api connection, search photos

---
+++ node_section/config.js
@@ -0,0 +1,3 @@
+// this is where i'll place my flickr api credentials
+// -----
+// module.export this and require it where needed [ srever.js?]
---
+++ node_section/routes.js
@@ -0,0 +1,1 @@
+// maybe pull routes from server.js and place them in here to modulize my app
