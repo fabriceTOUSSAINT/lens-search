c5fb053f9af8dd2cd6c09a6f0a678d05201f17b0
Fabrice Toussaint <Fabrice@macs-MBP.home>
2016-12-26 12:35:09 -0500

    new base

--- README.md
+++
@@ -1,6 +0,0 @@
-# lens-search
-An application where you can search and learn more about particular photography lens to find the best that fit for you.
-
-## Built with
-| In progress
-* Flickr api
--- server/routes/lens.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as LensController from '../controllers/lens.controller';
-const router = new Router();
-
-// Get all Lenss
-router.route('/lens').get(LensController.getLens);
-
-
-export default router;
