c80cbd6e2a2f3b90382b97fd5c5decc2db93ec59
Fabrice Toussaint <fabricetoussaint@Fabrices-MacBook-Pro.local>
2020-08-30 13:41:24 -0400

    mvp designs up, need to improve algo

--- client/src/containers/Hero/index.tsx
+++ client/src/containers/Hero/index.tsx
@@ -7,17 +7,47 @@ import defaultBackground from '../../assets/lens_jawn_bg.jpg';
 
 const Hero = () => {
   const { data } = useStoreSelectedLensQuery();
+
+  const { getSelectedLens } = { ...data };
+  const {
+    lensName,
+    fStopMin,
+    fStopMax,
+    focalLength,
+    lensMount,
+    msrp,
+    msrp_002,
+  } = { ...getSelectedLens };
+
   return (
     <HeroWrapper>
       <BackgroundLeft bg={defaultBackground} />
       <BackgroundRight />
-      <LensInfo>{data?.getSelectedLens?.lensName} </LensInfo>
+      <LensInfo>
+        {getSelectedLens ? (
+          <>
+            <LensInfoDetail>{lensName}</LensInfoDetail>
+            <LensInfoDetail>{`F/ stop ${fStopMin} - ${fStopMax}`}</LensInfoDetail>
+            <LensInfoDetail>{`${lensMount} Mount`}</LensInfoDetail>
+            <LensInfoDetail>{`${focalLength}mm`}</LensInfoDetail>
+            <LensInfoDetail>{`MSRP: $${msrp}`}</LensInfoDetail>
+          </>
+        ) : (
+          <LensInfoDetail>Search for a lens!</LensInfoDetail>
+        )}
+      </LensInfo>
     </HeroWrapper>
   );
 };
 
 export default Hero;
 
+const LensInfoDetail = styled.h1`
+  color: #000;
+  font-size: 1.7rem;
+  line-height: 1.6rem;
+  font-weight: 800;
+`;
 const BackgroundLeft = styled.div<{ bg: any }>`
   width: 65vw;
   height: 400px;
@@ -34,14 +64,12 @@ const HeroWrapper = styled.div`
   background: #60becc;
 `;
 
-const LensInfo = styled.h1`
+const LensInfo = styled.div`
   top: 60px;
   right: 300px;
   background: #fff;
-  color: #000;
-  min-width: 600px;
-  padding: 12px 18px;
-  font-size: 2rem;
+  min-width: 400px;
+  padding: 40px 60px;
   border-radius: 4px;
   position: absolute;
 `;
