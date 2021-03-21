ecc5c2a6abbb42f96737031bf7e101c2cc66eb23
Fabrice Toussaint <fabricetoussaint@Fabrices-MacBook-Pro.local>
2021-03-21 15:06:53 -0400

    connect docker to internal db

--- server/src/knex.ts
+++ server/src/knex.ts
@@ -1,23 +1,12 @@
 // TODO: figure out how to spin up postgres image to connect to
-// const knexConfig: any = {
-//   client: process.env.KNEX_CLIENT,
-//   debug: process.env.NODE_ENV === 'development',
-//   connection: {
-//     host: process.env.KNEX_HOST,
-//     user: process.env.KNEX_USER,
-//     password: process.env.KNEX_PW,
-//     database: process.env.KNEX_DB,
-//   },
-// };
-
 const knexConfig: any = {
-  client: 'pg',
-  debug: false,
+  client: process.env.KNEX_CLIENT,
+  debug: process.env.NODE_ENV === 'development',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: '',
-    database: 'lens_search',
+    host: process.env.KNEX_HOST,
+    user: process.env.KNEX_USER,
+    password: process.env.KNEX_PW,
+    database: process.env.KNEX_DB,
   },
 };
 
